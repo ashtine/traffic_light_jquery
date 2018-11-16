40011a1e407b072f43b1d8889b9e3f074943a364
ashtine <ashtinetapanes@gmail.com>
2018-11-16 11:36:56 -0500

    added the toggle class and made functions anon

--- js/index.js
+++ js/index.js
@@ -1,18 +1,23 @@
 //Implement the red light using jQuery. Don't forget to add the script tags.
+//Toggles red light
+$("#stopButton").click(function() {
+	$("#stopLight.bulb").toggleClass("bulb-red");
+})
 
-function red() {
-    $("#stopLight.bulb").css("backgroundColor", "red");
-}
-
-    $("#stopButton").click(red);
-
-function yellow() {
-    $("#slowLight.bulb").css("backgroundColor", "yellow");
-}
-
-    $("#slowButton").click(yellow);
+$("#slowButton").click(function() {
+	$("#slowLight.bulb").toggleClass("bulb-yellow");
+})
 
 function green() {
     $("#goLight").css("backgroundColor", "green");
 }
     $("#goButton").click(green);
+
+function black() {
+	
+}   
+
+// $(“#stopLight.bulb”).click(function() {
+// $(this).toggleClass(.bulb)
+
+// $(#stopButton”).click (red);
